acd5c1961fe3198ab2e29c0c76509ce922fc58b6
Luiz Felipe Ornelas Passoni <luizfelipeornelasp@gmail.com>
2026-01-21 23:11:51 -0300

    init

---
+++ src/main/java/com/caixa/PoCVault/PoCVaultApplication.java
@@ -0,0 +1,14 @@
+package com.caixa.PoCVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoCVaultApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Caralha");
+        SpringApplication.run(PoCVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caixa/PoCVault/PoCVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caixa.PoCVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoCVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
